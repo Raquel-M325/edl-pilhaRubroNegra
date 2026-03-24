2b6fe04ae93fc1e5afe9ae6b2101de5029622a28
Ana Barbosa (Escolar) <nokisegunda@gmail.com>
2026-03-24 13:31:49 +0000

    fix: adiciona estrutura ao projeto

--- src/main/PilhaArrayRubroNegra.java
+++ src/main/PilhaArrayRubroNegra.java
@@ -13,12 +13,17 @@ public class PilhaArrayRubroNegra implements PilhaRubroNegra {
         arr = new Object[capacidade];
     }
 
-    public void pushR(Object o){ // Adiciona um elemento no final do array rubro (esq. para dir)
-        // Implemente aqui
+    public void pushR(Object o){
+        if (topRubro +1 == topNegro){
+            grow();
+        }
+        arr[++topRubro] = o;
     }
-
-    public void pushN(Object o){ // Adiciona um elemento no final da array negro (dir. para esq)
-        // Implemente aqui
+    public void pushN(Object o){
+        if (topRubro+1 == topNegro){
+            grow();
+        }
+        arr[--topNegro] = o;
     }
 
     public Object popR() throws PilhaVaziaExcecao{ // Remove e retorna o ultimo elemento do array rubro
@@ -31,8 +36,22 @@ public class PilhaArrayRubroNegra implements PilhaRubroNegra {
         return n;
     }
 
-    public void grow() { // Aumenta a capacidade (duplicidade) do array geral, mantendo o array negro mais a direita
+    public void grow() {
+        int n_cap = capacidade*2;
+        Object n_arr[] = new Object[n_cap];
+
+        for(int i=0; i <= topRubro; i++)
+            n_arr[i] = arr[i];
         
+        int tamNegro = capacidade - topNegro;
+        int n_topNegro = n_cap - tamNegro;
+
+        for(int i=0; i < tamNegro; i++)
+            n_arr[n_topNegro + i] = arr[topNegro + i];
+
+        topNegro = n_topNegro;
+        capacidade = n_cap;
+        arr = n_arr;
     }
 
     public void shirink() { // Diminui o array geral pela metade, mantendo o array negro mais a direita
