c98fd6c4e5804f40fc1b610aef842e71d67a3283
Raquel <raquelmartiniano14@gmail.com>
2026-03-24 21:56:24 +0000

    falta só o shrink

--- src/main/java/PilhaArrayRubroNegra.java
+++ src/main/java/PilhaArrayRubroNegra.java
@@ -15,15 +15,19 @@ public class PilhaArrayRubroNegra implements PilhaRubroNegra {
     // Métodos
 
     public void pushR(Object o){ // Adiciona um elemento no final do array rubro
-        if (topR()){
+        if (topRubro + 1 == topNegro){
             grow();
         }
+
+        arr[++topRubro] = o; //"o" é o valor, vai continuar crescendo
     }
 
     public void pushN(Object o){ // Adiciona um elemento no final do array negro
-        if (topN()){
+        if (topNegro - 1 == topRubro){
             grow();
-        }
+        } 
+
+        arr[--topNegro] = o; //cresce de forma invertida, esse sinal primeiro faz que cresça e depois o indice do elemento colocado
     }
 
     public Object popR() throws PilhaVaziaExcecao{ // Remove e retorna o ultimo elemento do array rubro
@@ -39,15 +43,32 @@ public class PilhaArrayRubroNegra implements PilhaRubroNegra {
             throw new PilhaVaziaExcecao("A pilha preta está vazia");
 
         }
-        return arr[topNegro++]; //já que está indo ao contrário, então para retirar precisa ir para direita/positivo
+        return arr[topNegro++]; //já que está indo ao contrário, então para retirar precisa ir para direita/positivo, pega primeiro o elemento e depois tira naquela direção
     }
 
     public void grow() {
-        
+        int novo_capacidade = capacidade *= 2;
+        Object novo_arr[] = new Object[capacidade];
+    
+        for (int i = 0; i <= topRubro; i++){
+            novo_arr[i] = arr[i];
+        }
+
+        int tamanho_arr_P = capacidade - topNegro; //para saber o tamanho exato da lista preta, usarei de forma temporária
+        int novo_top_P = novo_capacidade - tamanho_arr_P; //para inverter de forma crescente! sendo um novo topNegro
+
+        for (int i = 0; i <= tamanho_arr_P; i++){
+            novo_arr[novo_top_P + i] = arr[novo_top_P + i];
+        }
+
+        arr = novo_arr;
+        capacidade = novo_capacidade;
+        topNegro = novo_top_P;
+
     }
 
     public void shrink() { // Diminui o array geral pela metade, mantendo o array negro mais a direita
-
+        
     }
 
     public Object topR() throws PilhaVaziaExcecao{ // Retorna o ultimo elemento do array rubro
